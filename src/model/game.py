03bb1fbede0b2f579e767633e2c748371ed99eed
mymusise <mymusise1@gmail.com>
2021-01-26 22:52:23 +0800

    add start class select
    Signed-off-by: mymusise <mymusise1@gmail.com>

--- src/model/game.py
+++ src/model/game.py
@@ -1,3 +1,4 @@
+from django.core.validators import EMPTY_VALUES
 from tensorflow.core.framework.node_def_pb2 import _NODEDEF_EXPERIMENTALDEBUGINFO
 import re
 from .tokens import TALKING_TOKEN, NONTALKING_TOKEN
@@ -38,21 +39,27 @@ class Player(object):
 class GameController(object):
     PRE_TEXT_ACTION = "> "
     END_ACTION_ADD = "。"
+    CUSTOM_START_ID = -1
+    EMPTY_SCRIPT = {0: ""}
 
-    def __init__(self, player_name, step):
+    def __init__(self, player_name, step, start_id):
         self.player = Player(player_name)  # 应该初始化更多的用户信息
         self.step = step
-        # self.scripts = {
-        #     0: f"你叫{self.player.name}，是一名剑修，没有门派，没有师傅。小时候在一次巧合下在村子外面捡到一本剑法秘籍，通过十年的琢磨，{self.player.name}终于把秘籍修炼完了。于是{self.player.name}离开了村子，想在外面历练一番。离开村子后{self.player.name}来到一片树林，碰到一位在砍柴的樵夫。",
-        #     1: f"{self.player.name}在树林的边上发现一间客栈，进去发现里面坐满了人，向小二打听了下发现大部分都是来参加比武大赛的。",
-        #     10: f"一天忙活下来后，{self.player.name}感觉有点疲倦，便回到客栈休息去了。第二天大早，{self.player.name}听到有人在敲门，开门一看，是一位亭亭玉立的小女孩。",
-        #     15: f"{self.player.name}突然感觉背后一凉，后脑勺传来一股力量，眼前一黑，晕了过去。醒来后{self.player.name}发现自己躺在在一个乌黑的山洞里，山洞的石壁上闪烁着微弱的光，看上去视乎像一些荧光的草。正在{self.player.name}想站起来时候，发现自己脚被铁链绑住了，铁链的另一头拴在山洞的石壁上。",
-        #     16: f"这个时候，一位白发苍苍的老者出现在{self.player.name}面前。",
-        #     20: f"那位老者离开前，扔给了{self.player.name}一本秘籍，{self.player.name}打开后发现原来这本秘籍是之前自己修炼的剑法的进阶。{self.player.name}坐了下来，静静地领悟秘籍中的剑法，不知道过了多长时间，{self.player.name}站了起来，试了秘籍中的前两式。",
-        # }
-        self.scripts = {
-            0: f"漆黑中醒来，{self.player.name}发现自己在一个完全陌生的树林中，四面一片漆黑。{self.player.name}此时内心充满了恐惧与不安，他不知道自己如何到这个地方。正在他伸手打算去摸一下自己的口袋，然而发现他的口袋中什么都没有，他决定去找到自己为什么会出现在这里。{self.player.name}站起身来望了望四周，发现有一条小路。"
-        }
+
+        self.init_script(start_id)
+
+    def init_script(self, start_id):
+        if start_id == self.CUSTOM_START_ID:
+            self.scripts = self.EMPTY_SCRIPT
+            return
+
+        if start_id not in GAME_SCRIPTS:
+            raise ValueError(f"start_id:{start_id} is not in GAME_SCRIPTS")
+
+        scripts = {}
+        for k, v in GAME_SCRIPTS[start_id]['content'].items():
+            scripts[k] = v.format(player_name=self.player.name)
+        self.scripts = scripts
 
     @property
     def scene(self):
@@ -86,3 +93,33 @@ class GameController(object):
         if steps in self.scripts:
             return self.scripts.get(steps, None)
         return ''
+
+
+GAME_SCRIPTS = {
+    1: {
+        "name": "乞丐",
+        "describe": "",
+        "content": {
+            0: "漆黑中醒来，{player_name}发现自己在一个完全陌生的树林中，四面一片漆黑。{player_name}此时内心充满了恐惧与不安，他不知道自己如何到这个地方。正在他伸手打算去摸一下自己的口袋，然而发现他的口袋中什么都没有，他决定去找到自己为什么会出现在这里。{player_name}站起身来望了望四周，发现有一条小路。",
+        }
+    },
+    2: {
+        "name": "商人",
+        "describe": "",
+        "content": {
+            0: "你叫{player_name}，是一名商人。",
+        }
+    },
+    3: {
+        "name": "剑修",
+        "describe": "",
+        "content": {
+            0: "你叫{player_name}，是一名剑修，没有门派，没有师傅。小时候在一次巧合下在村子外面捡到一本剑法秘籍，通过十年的琢磨，{player_name}终于把秘籍修炼完了。于是{player_name}离开了村子，想在外面历练一番。离开村子后{player_name}来到一片树林，碰到一位在砍柴的樵夫。",
+            2: "{player_name}在树林的边上发现一间客栈，进去发现里面坐满了人，向小二打听了下发现大部分都是来参加比武大赛的。",
+            5: "一天忙活下来后，{player_name}感觉有点疲倦，便回到客栈休息去了。第二天大早，{player_name}听到有人在敲门，开门一看，是一位亭亭玉立的小女孩。",
+            12: "{player_name}突然感觉背后一凉，后脑勺传来一股力量，眼前一黑，晕了过去。醒来后{player_name}发现自己躺在在一个乌黑的山洞里，山洞的石壁上闪烁着微弱的光，看上去视乎像一些荧光的草。正在{player_name}想站起来时候，发现自己脚被铁链绑住了，铁链的另一头拴在山洞的石壁上。",
+            15: "这个时候，一位白发苍苍的老者出现在{player_name}面前。",
+            20: "那位老者离开前，扔给了{player_name}一本秘籍，{player_name}打开后发现原来这本秘籍是之前自己修炼的剑法的进阶。{player_name}坐了下来，静静地领悟秘籍中的剑法，不知道过了多长时间，{player_name}站了起来，试了秘籍中的前两式。",
+        }
+    }
+}
--- src/model/views.py
+++ src/model/views.py
@@ -4,16 +4,27 @@ from rest_framework import serializers
 from rest_framework.decorators import action
 from rest_framework.response import Response
 from .text_generator import TextGenerator
-from .game import GameController
+from .game import GameController, GAME_SCRIPTS
 from apis.settings import logger
 from utils.base import BaseView
 
 
+class StartInfoView(BaseView, viewsets.GenericViewSet):
+
+    @action(detail=True)
+    def get_start_info(self, request):
+        result = [{"id": k, "name": v['name'], "describe": v['describe']}
+                  for k, v in GAME_SCRIPTS.items()]
+        return Response(result)
+
+
 class TextGeneratorSerializer(serializers.Serializer):
     history = serializers.ListField()
-    input_text = serializers.CharField(max_length=128, required=False, allow_blank=True)
+    input_text = serializers.CharField(
+        max_length=128, required=False, allow_blank=True)
     steps = serializers.IntegerField()
     text_type = serializers.ChoiceField(choices=['action', 'say'])
+    start_id = serializers.IntegerField(default=1)
 
 
 class TextGeneratorView(BaseView, viewsets.GenericViewSet):
@@ -28,11 +39,12 @@ class TextGeneratorView(BaseView, viewsets.GenericViewSet):
         input_text = serializer.data.get('input_text')
         text_type = serializer.data.get('text_type')
         step = serializer.data.get('steps')
+        start_id = serializer.data.get('start_id', 1)
 
-        game = GameController(self.default_player_name, step)
+        game = GameController(self.default_player_name, step, start_id)
 
         current_text = game.wrap_text(input_text, text_type=text_type)
-        
+
         given_text = game.get_given_steps(step)
         if given_text:
             add_scene = False
@@ -41,10 +53,11 @@ class TextGeneratorView(BaseView, viewsets.GenericViewSet):
 
         next_text = ''
         if step > 0:
-            generator = TextGenerator(serializer.data.get('history'), game.scene)
-            next_text = generator.gen_next(game.clean_warp(current_text), text_type, game.player, add_scene)
+            generator = TextGenerator(
+                serializer.data.get('history'), game.scene)
+            next_text = generator.gen_next(game.clean_warp(
+                current_text), text_type, game.player, add_scene)
 
-        
         if given_text is not None:
             next_text += given_text
 
